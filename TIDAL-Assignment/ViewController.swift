6162653a035e360b88836a2765390f2b56ff09e6
Alberto Sendra Estrella <alberto@albertosendra.com>
2022-08-16 07:53:08 +0200

    - Initial commit

---
+++ TIDAL-Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TIDAL-Assignment
+//
+//  Created by Alberto Sendra Estrella on 16/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
